85073b94d58754f657e56d6d57ed6da7cd1af65b
neilbernard-sscgi <nbsupillo@sscgi.com>
2025-03-14 11:32:00 +0800

    replace activity.js with preparation.js and add initial game logic

---
+++ discussion/preparation.js
@@ -0,0 +1,17 @@
+console.log("Welcome to pokemon arena");
+
+let role = 1;
+console.log(
+  "what do you want to do\n1 - Join the battle\n2 - Watch\n3 - Custom battle"
+);
+
+console.log("Pick a tournament you want to participate:\n1 - 3");
+console.log();
+if (role == 1) {
+  console.log("Pick a pokemon");
+}
+/**
+ * pick a tournament type
+ * pick a pokemon
+ * start
+ */
